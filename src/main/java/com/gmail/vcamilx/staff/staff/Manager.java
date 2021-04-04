20d781901c4d8c3bce2088a0e44b817fd42f6b90
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-04-03 22:46:25 -0500

    Bug solved and changed a Map to List
    The staff chat system has been changed and replaced by a list, as the Map was not being used.

--- src/main/java/com/gmail/vcamilx/staff/staff/Manager.java
+++ src/main/java/com/gmail/vcamilx/staff/staff/Manager.java
@@ -1,20 +1,21 @@
 package com.gmail.vcamilx.staff.staff;
 
 import com.gmail.vcamilx.staff.Staff;
+import com.gmail.vcamilx.staff.staff.inventory.StaffInventory;
 import com.gmail.vcamilx.staff.util.chat.ChatUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 public class Manager {
-    private final Map<UUID, Player> staffMap = new HashMap<>();
-    private final Map<UUID, Player> staffChat = new HashMap<>();
+    private static final Map<UUID, Player> staffMap = new HashMap<>();
+    private static final List<UUID> staffChat = new ArrayList<>();
+
     private final Map<UUID, ItemStack[]> playerInventoryHashMap = new HashMap<>();
+    private final StaffInventory staffInventory = new StaffInventory();
 
     public void setStaff(Player player) {
         if (!isStaffMode(player)) {
@@ -22,6 +23,7 @@ public class Manager {
             playerInventoryHashMap.put(player.getUniqueId(), player.getInventory().getContents());
             player.getInventory().clear();
             player.setGameMode(GameMode.CREATIVE);
+            staffInventory.inventory(player);
             for (Player players : Bukkit.getOnlinePlayers()) {
                 if (!players.hasPermission("staff.mode")) {
                     players.hidePlayer(Staff.getPlugin(), player);
@@ -42,9 +44,14 @@ public class Manager {
     public void disableStaff(Player player) {
         if (isStaffMode(player)) {
             staffMap.remove(player.getUniqueId());
-            player.getInventory().setContents(playerInventoryHashMap.get(player.getUniqueId()));
-            playerInventoryHashMap.remove(player.getUniqueId());
+
+            if (playerInventoryHashMap.containsKey(player.getUniqueId())) {
+                player.getInventory().setContents(playerInventoryHashMap.get(player.getUniqueId()));
+                playerInventoryHashMap.remove(player.getUniqueId());
+            }
+
             player.setGameMode(GameMode.SURVIVAL);
+
             for (Player players : Bukkit.getOnlinePlayers()) {
                 if (!players.canSee(player)) {
                     players.showPlayer(Staff.getPlugin(), player);
@@ -63,7 +70,7 @@ public class Manager {
 
     public void setStaffChat(Player player) {
         if (!isStaffChat(player)) {
-            staffChat.put(player.getUniqueId(), player);
+            staffChat.add(player.getUniqueId());
             player.sendMessage(ChatUtil.color("&bNow you're in staff-chat!"));
             return;
         }
@@ -77,7 +84,6 @@ public class Manager {
     }
 
     public boolean isStaffChat(Player player) {
-        return staffChat.containsKey(player.getUniqueId());
+        return staffChat.contains(player.getUniqueId());
     }
-
 }
