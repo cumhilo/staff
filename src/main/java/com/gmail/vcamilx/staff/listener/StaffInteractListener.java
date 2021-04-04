d29fc1ad956f9ac121f1029212368c6ff21089f3
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-04-03 23:01:07 -0500

    the listener StaffInteractListener has been created
    This class has a long way to go and it doesn't work LOL, but well, I know the cause, in future commits it will be fixed.

---
+++ src/main/java/com/gmail/vcamilx/staff/listener/StaffInteractListener.java
@@ -0,0 +1,34 @@
+package com.gmail.vcamilx.staff.listener;
+
+import com.gmail.vcamilx.staff.staff.Manager;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class StaffInteractListener implements Listener {
+    private final Manager manager = new Manager();
+
+    @EventHandler
+    public void interaction(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        Action action = event.getAction();
+
+        if (manager.isStaffMode(player)) {
+            if (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK) {
+                if (player.getInventory().getItemInMainHand().getType() == Material.ENDER_EYE) {
+                    if (Bukkit.getOnlinePlayers().size() > 1) {
+                        for (Player players : Bukkit.getOnlinePlayers()) {
+                            if (players != null && !manager.isStaffMode(players)) {
+                                player.teleport(players.getLocation());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
