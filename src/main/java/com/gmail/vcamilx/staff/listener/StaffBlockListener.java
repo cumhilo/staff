6e55bb716974bc7449bcd3facfbe45982cf17928
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-04-03 22:47:54 -0500

    StaffBlockListener added
    A listener has been created to place blocks in staff mode, and prevent placing or removing blocks.

---
+++ src/main/java/com/gmail/vcamilx/staff/listener/StaffBlockListener.java
@@ -0,0 +1,31 @@
+package com.gmail.vcamilx.staff.listener;
+
+import com.gmail.vcamilx.staff.staff.Manager;
+import com.gmail.vcamilx.staff.util.chat.ChatUtil;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class StaffBlockListener implements Listener {
+    private final Manager manager = new Manager();
+
+    @EventHandler
+    public void onPlaceBlock(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+        if (manager.isStaffMode(player)) {
+            player.sendMessage(ChatUtil.color("&cYou cannot place bocks when you are in staff mode!"));
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onBreakBlock(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        if (manager.isStaffMode(player)) {
+            player.sendMessage(ChatUtil.color("&cYou cannot break bocks when you are in staff mode!"));
+            event.setCancelled(true);
+        }
+    }
+}
