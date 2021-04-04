c47e01115a0756ba77d0d13c04afbb09243c3d59
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-04-03 22:53:09 -0500

    An unnecessary for loop has been removed and an instance has been fixed.
    The event has been modified when a player enters, an if has been implemented to check whether the player is in staff mode or not.

--- src/main/java/com/gmail/vcamilx/staff/listener/StaffJoinListener.java
+++ src/main/java/com/gmail/vcamilx/staff/listener/StaffJoinListener.java
@@ -1,23 +1,22 @@
 package com.gmail.vcamilx.staff.listener;
 
 import com.gmail.vcamilx.staff.staff.Manager;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class StaffJoinListener implements Listener {
-    private Manager manager;
+    private final Manager manager = new Manager();
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void staffJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        for (Player players : Bukkit.getOnlinePlayers()) {
-
-        }
         if (player.hasPermission("staff.mode")) {
-            manager.setStaff(player);
+            if (!manager.isStaffMode(player)) {
+                manager.setStaff(player);
+            }
         }
     }
 }
