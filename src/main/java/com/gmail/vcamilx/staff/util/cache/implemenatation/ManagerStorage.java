2756da73316c324977b636227e1cbae2bc446a69
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-04-15 18:14:02 -0500

    Some methods have been overwritten

--- src/main/java/com/gmail/vcamilx/staff/util/cache/implemenatation/ManagerStorage.java
+++ src/main/java/com/gmail/vcamilx/staff/util/cache/implemenatation/ManagerStorage.java
@@ -5,6 +5,7 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 public class ManagerStorage implements IStorage<UUID, ItemStack[]> {
@@ -14,5 +15,31 @@ public class ManagerStorage implements IStorage<UUID, ItemStack[]> {
     @Override
     public Map<UUID, ItemStack[]> get() {
         return staffManager;
+    }
+
+
+    @Override
+    public void add(UUID key, ItemStack[] value) {
+        this.get().put(key, value);
+    }
+
+    @Override
+    public void remove(UUID key) {
+        this.get().remove(key);
+    }
+
+    @Override
+    public Optional<ItemStack[]> find(UUID key) {
+        return Optional.ofNullable(this.get().get(key));
+    }
+
+    @Override
+    public ItemStack[] getValue(UUID key) {
+        return get().get(key);
+    }
+
+    @Override
+    public boolean exists(UUID key) {
+        return this.get().containsKey(key);
     }
 }
