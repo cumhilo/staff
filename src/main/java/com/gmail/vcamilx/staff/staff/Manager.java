7ffcd32c96642a449edced11fd497e28617708e3
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-04-15 18:13:37 -0500

    @InjectIgnore and some other things have been modified.

--- src/main/java/com/gmail/vcamilx/staff/staff/Manager.java
+++ src/main/java/com/gmail/vcamilx/staff/staff/Manager.java
@@ -5,10 +5,12 @@ import com.gmail.vcamilx.staff.staff.inventory.StaffInventory;
 import com.gmail.vcamilx.staff.util.cache.implemenatation.ManagerStorage;
 import com.gmail.vcamilx.staff.util.chat.ChatUtil;
 import me.yushust.inject.InjectAll;
+import me.yushust.inject.InjectIgnore;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 
+import javax.inject.Named;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -17,48 +19,41 @@ import java.util.UUID;
 public class Manager {
 
     private Staff staff;
-    private ManagerStorage playerCache;
-    private final StaffInventory staffInventory = new StaffInventory();
-    private static final List<UUID> staffChat = new ArrayList<>();
+
+    @InjectIgnore private final StaffInventory staffInventory = new StaffInventory();
+    @InjectIgnore private static final List<UUID> staffChat = new ArrayList<>();
+
+    @Named("manager-storage")
+    private ManagerStorage managerStorage;
 
     public void setStaff(Player player) {
-        // Inventory
-        playerCache.add(player.getUniqueId(), player.getInventory().getContents());
-        player.getInventory().clear();
-        staffInventory.inventory(player);
+        if (!isStaffMode(player)) {
+            managerStorage.add(player.getUniqueId(), player.getInventory().getContents());
+            player.getInventory().clear();
+            staffInventory.inventory(player);
 
-        player.setGameMode(GameMode.CREATIVE);
+            player.setGameMode(GameMode.CREATIVE);
+            setVanish(player);
 
-        // Vanish
-        for (Player players : Bukkit.getOnlinePlayers()) {
-            if (!players.hasPermission("staff.mode")) {
-                players.hidePlayer(staff, player);
-            }
+            player.sendTitle(
+                    ChatUtil.color(staff.getConfig().getString("messages.staff.join.title.title")),
+                    ChatUtil.color(staff.getConfig().getString("messages.staff.join.title.subtitle")),
+                    staff.getConfig().getInt("messages.staff.join.title.fadeIn"),
+                    staff.getConfig().getInt("messages.staff.join.title.stay"),
+                    staff.getConfig().getInt("messages.staff.join.title.fadeOut"));
+            player.sendMessage(
+                    ChatUtil.color(staff.getConfig().getString("messages.staff.join.chat")));
+            return;
         }
-        // Messages
-        player.sendTitle(
-                ChatUtil.color(staff.getConfig().getString("messages.staff.join.title.title")),
-                ChatUtil.color(staff.getConfig().getString("messages.staff.join.title.subtitle")),
-                staff.getConfig().getInt("messages.staff.join.title.fadeIn"),
-                staff.getConfig().getInt("messages.staff.join.title.stay"),
-                staff.getConfig().getInt("messages.staff.join.title.fadeOut"));
-        player.sendMessage(
-                ChatUtil.color(staff.getConfig().getString("messages.staff.join.chat")));
-    }
 
-    public void disableStaff(Player player) {
-        playerCache.find(player.getUniqueId()).ifPresent(itemStacks -> {
-            player.getInventory().setContents(playerCache.getValue(player.getUniqueId()));
-            playerCache.remove(player.getUniqueId());
+        managerStorage.find(player.getUniqueId()).ifPresent(itemStacks -> {
+            player.getInventory().setContents(managerStorage.getValue(player.getUniqueId()));
+            managerStorage.remove(player.getUniqueId());
         });
 
         player.setGameMode(GameMode.SURVIVAL);
 
-        for (Player players : Bukkit.getOnlinePlayers()) {
-            if (!players.canSee(player)) {
-                players.showPlayer(staff, player);
-            }
-        }
+        setVanish(player);
 
         player.sendTitle(
                 ChatUtil.color(staff.getConfig().getString("messages.staff.leave.title.title")),
@@ -68,7 +63,16 @@ public class Manager {
                 staff.getConfig().getInt("messages.staff.leave.title.fadeOut"));
         player.sendMessage(
                 ChatUtil.color(staff.getConfig().getString("messages.staff.leave.chat")));
+    }
 
+    private void setVanish(Player player) {
+        for (Player players : Bukkit.getOnlinePlayers()) {
+            if (!players.hasPermission("staff.mode") && players.canSee(player)) {
+                players.hidePlayer(staff, player);
+                return;
+            }
+            players.showPlayer(staff, player);
+        }
     }
 
     public void setStaffChat(Player player) {
@@ -82,11 +86,11 @@ public class Manager {
         player.sendMessage(ChatUtil.color("&cYou aren't in staff chat now!"));
     }
 
-    public boolean isStaffMode(Player player) {
-        return playerCache.exists(player.getUniqueId());
-    }
-
     public boolean isStaffChat(Player player) {
         return staffChat.contains(player.getUniqueId());
+    }
+
+    public boolean isStaffMode(Player player) {
+        return managerStorage.exists(player.getUniqueId());
     }
 }
