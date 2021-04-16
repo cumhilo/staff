832bd9f47c0b317b245c8130aedf073c0878b85e
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-04-16 11:57:25 -0500

    Unnecessary annotations have been removed

--- src/main/java/com/gmail/vcamilx/staff/util/cache/implemenatation/ManagerStorage.java
+++ src/main/java/com/gmail/vcamilx/staff/util/cache/implemenatation/ManagerStorage.java
@@ -12,33 +12,26 @@ public class ManagerStorage implements IStorage<UUID, ItemStack[]> {
 
     private final Map<UUID, ItemStack[]> staffManager = new HashMap<>();
 
-    @Override
     public Map<UUID, ItemStack[]> get() {
         return staffManager;
     }
 
-
-    @Override
     public void add(UUID key, ItemStack[] value) {
         this.get().put(key, value);
     }
 
-    @Override
     public void remove(UUID key) {
         this.get().remove(key);
     }
 
-    @Override
     public Optional<ItemStack[]> find(UUID key) {
         return Optional.ofNullable(this.get().get(key));
     }
 
-    @Override
     public ItemStack[] getValue(UUID key) {
         return get().get(key);
     }
 
-    @Override
     public boolean exists(UUID key) {
         return this.get().containsKey(key);
     }
