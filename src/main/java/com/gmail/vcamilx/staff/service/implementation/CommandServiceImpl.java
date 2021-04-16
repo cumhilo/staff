4c162b4922d017deea9568bf0f98df1e667c1844
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-04-16 11:59:20 -0500

    FreezeCommand was added

--- src/main/java/com/gmail/vcamilx/staff/service/implementation/CommandServiceImpl.java
+++ src/main/java/com/gmail/vcamilx/staff/service/implementation/CommandServiceImpl.java
@@ -1,10 +1,7 @@
 package com.gmail.vcamilx.staff.service.implementation;
 
 import com.gmail.vcamilx.staff.Staff;
-import com.gmail.vcamilx.staff.command.CheckStaffCommand;
-import com.gmail.vcamilx.staff.command.InventorySeeCommand;
-import com.gmail.vcamilx.staff.command.StaffChatCommand;
-import com.gmail.vcamilx.staff.command.StaffCommand;
+import com.gmail.vcamilx.staff.command.*;
 import com.gmail.vcamilx.staff.service.IService;
 import me.yushust.inject.InjectAll;
 
@@ -14,17 +11,19 @@ import java.util.Objects;
 public class CommandServiceImpl implements IService {
 
     private Staff staff;
+
     private CheckStaffCommand checkStaffCommand;
     private InventorySeeCommand inventorySeeCommand;
     private StaffChatCommand staffChatCommand;
     private StaffCommand staffCommand;
-
+    private FreezeCommand freezeCommand;
 
     private void registerCommand() {
         Objects.requireNonNull(staff.getCommand("check")).setExecutor(checkStaffCommand);
         Objects.requireNonNull(staff.getCommand("inventorysee")).setExecutor(inventorySeeCommand);
         Objects.requireNonNull(staff.getCommand("staffchat")).setExecutor(staffChatCommand);
         Objects.requireNonNull(staff.getCommand("staff")).setExecutor(staffCommand);
+        Objects.requireNonNull(staff.getCommand("freeze")).setExecutor(freezeCommand);
     }
 
     @Override
