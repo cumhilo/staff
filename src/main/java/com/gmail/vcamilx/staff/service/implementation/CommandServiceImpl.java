38b36fa9be4c9439cdbf6c470392a9beba4d7c83
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-04-25 22:04:19 -0500

    Removed NonNull require from getCommands

--- src/main/java/com/gmail/vcamilx/staff/service/implementation/CommandServiceImpl.java
+++ src/main/java/com/gmail/vcamilx/staff/service/implementation/CommandServiceImpl.java
@@ -9,8 +9,6 @@ import com.gmail.vcamilx.staff.command.StaffCommand;
 import com.gmail.vcamilx.staff.service.IService;
 import me.yushust.inject.InjectAll;
 
-import java.util.Objects;
-
 @InjectAll
 public class CommandServiceImpl implements IService {
 
@@ -23,11 +21,11 @@ public class CommandServiceImpl implements IService {
     private FreezeCommand freezeCommand;
 
     private void registerCommand() {
-        Objects.requireNonNull(staff.getCommand("check")).setExecutor(checkStaffCommand);
-        Objects.requireNonNull(staff.getCommand("inventorysee")).setExecutor(inventorySeeCommand);
-        Objects.requireNonNull(staff.getCommand("staffchat")).setExecutor(staffChatCommand);
-        Objects.requireNonNull(staff.getCommand("staff")).setExecutor(staffCommand);
-        Objects.requireNonNull(staff.getCommand("freeze")).setExecutor(freezeCommand);
+        staff.getCommand("check").setExecutor(checkStaffCommand);
+        staff.getCommand("inventorysee").setExecutor(inventorySeeCommand);
+        staff.getCommand("staffchat").setExecutor(staffChatCommand);
+        staff.getCommand("staff").setExecutor(staffCommand);
+        staff.getCommand("freeze").setExecutor(freezeCommand);
     }
 
     @Override
