e147c62bbd21823a25f41931448eb11129f6c64c
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-04-25 22:03:45 -0500

    Changes, not yet finalized

--- src/main/java/com/gmail/vcamilx/staff/util/cache/implemenatation/ManagerStorage.java
+++ src/main/java/com/gmail/vcamilx/staff/util/cache/implemenatation/ManagerStorage.java
@@ -3,14 +3,14 @@ package com.gmail.vcamilx.staff.util.cache.implemenatation;
 import com.gmail.vcamilx.staff.util.cache.IStorage;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ManagerStorage implements IStorage<UUID, ItemStack[]> {
 
-    private final Map<UUID, ItemStack[]> staffManager = new HashMap<>();
+    private final Map<UUID, ItemStack[]> staffManager = new ConcurrentHashMap<>();
 
     public Map<UUID, ItemStack[]> get() {
         return staffManager;
