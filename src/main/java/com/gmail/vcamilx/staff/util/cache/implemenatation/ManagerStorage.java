ea2c30986d6888c0e4ca697bb9209bb71a766839
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-04-29 16:36:13 -0500

    👍

--- src/main/java/com/gmail/vcamilx/staff/util/cache/implemenatation/ManagerStorage.java
+++ src/main/java/com/gmail/vcamilx/staff/util/cache/implemenatation/ManagerStorage.java
@@ -4,7 +4,6 @@ import com.gmail.vcamilx.staff.util.cache.IStorage;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -14,25 +13,5 @@ public class ManagerStorage implements IStorage<UUID, ItemStack[]> {
 
     public Map<UUID, ItemStack[]> get() {
         return staffManager;
-    }
-
-    public void add(UUID key, ItemStack[] value) {
-        this.get().put(key, value);
-    }
-
-    public void remove(UUID key) {
-        this.get().remove(key);
-    }
-
-    public Optional<ItemStack[]> find(UUID key) {
-        return Optional.ofNullable(this.get().get(key));
-    }
-
-    public ItemStack[] getValue(UUID key) {
-        return get().get(key);
-    }
-
-    public boolean exists(UUID key) {
-        return this.get().containsKey(key);
     }
 }
