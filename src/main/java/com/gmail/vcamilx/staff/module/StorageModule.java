882d60f70d1266b028be9fe2880b0882ee6bb001
BuildTools <unconfigured@null.spigotmc.org>
2021-05-17 08:37:43 -0300

    Update StorageModule

--- src/main/java/com/gmail/vcamilx/staff/module/StorageModule.java
+++ src/main/java/com/gmail/vcamilx/staff/module/StorageModule.java
@@ -2,6 +2,7 @@ package com.gmail.vcamilx.staff.module;
 
 import com.gmail.vcamilx.staff.util.cache.IStorage;
 import com.gmail.vcamilx.staff.util.cache.implemenatation.ManagerStorage;
+import com.gmail.vcamilx.staff.util.cache.implemenatation.Storage;
 import me.yushust.inject.AbstractModule;
 import me.yushust.inject.key.TypeReference;
 import org.bukkit.inventory.ItemStack;
@@ -12,7 +13,10 @@ public class StorageModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(new TypeReference<IStorage<UUID, ItemStack[]>>() {
-        }).named("staff-storage").to(ManagerStorage.class).singleton();
+        bindStorage(UUID.class, ItemStack[].class);
+    }
+
+    private void bindStorage(Class<?> keyType, Class<?> valueType) {
+        bind(TypeReference.of(IStorage.class, keyType, valueType)).to(TypeReference.of(Storage.class, keyType, valueType)).singleton();
     }
 }
