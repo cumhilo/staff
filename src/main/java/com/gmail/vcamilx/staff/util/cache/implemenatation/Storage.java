c5db86178475dc32cacb17bffcb036a89d09f367
BuildTools <unconfigured@null.spigotmc.org>
2021-05-17 08:35:41 -0300

    Create Storage

---
+++ src/main/java/com/gmail/vcamilx/staff/util/cache/implemenatation/Storage.java
@@ -0,0 +1,22 @@
+package com.gmail.vcamilx.staff.util.cache.implemenatation;
+
+import com.gmail.vcamilx.staff.util.cache.IStorage;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * A generic implementation of {@link IStorage}.
+ *
+ * @param <K> The key type held by this {@link Storage}.
+ * @param <V> The value type held by this {@link Storage}.
+ */
+public class Storage<K, V> implements IStorage<K, V> {
+
+    private final ConcurrentMap<K, V> cache = new ConcurrentHashMap<>();
+
+    @Override public Map<K, V> get() {
+        return cache;
+    }
+}
