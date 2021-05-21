16f8803bf6f1cab7cabb1738a80616eb3be6c5e1
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-05-20 22:55:41 -0500

    version & database service has been added

--- plugin/src/main/java/com/github/vcamilx/staff/module/ServiceModule.java
+++ plugin/src/main/java/com/github/vcamilx/staff/module/ServiceModule.java
@@ -3,7 +3,9 @@ package com.github.vcamilx.staff.module;
 import com.github.vcamilx.staff.core.service.IService;
 import com.github.vcamilx.staff.service.StaffService;
 import com.github.vcamilx.staff.service.implementation.CommandServiceImpl;
+import com.github.vcamilx.staff.service.implementation.DatabaseServiceImpl;
 import com.github.vcamilx.staff.service.implementation.ListenerServiceImpl;
+import com.github.vcamilx.staff.service.implementation.VersionServiceImpl;
 import me.yushust.inject.AbstractModule;
 
 public class ServiceModule extends AbstractModule {
@@ -13,6 +15,8 @@ public class ServiceModule extends AbstractModule {
         bindService("staff-service", StaffService.class);
         bindService("command-service", CommandServiceImpl.class);
         bindService("listener-service", ListenerServiceImpl.class);
+        bindService("version-service", VersionServiceImpl.class);
+        bindService("database-service", DatabaseServiceImpl.class);
     }
 
     private void bindService(String name, Class<? extends IService> clazz) {
