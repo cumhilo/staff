2a3b1e56d0c24c0e2778b3d76f6aaa52dda6f707
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-05-20 22:47:42 -0500

    all new listeners have been added
    all new listeners have been added, they will be modified but not now, there will be many cam / improvements to come

--- plugin/src/main/java/com/github/vcamilx/staff/service/implementation/ListenerServiceImpl.java
+++ plugin/src/main/java/com/github/vcamilx/staff/service/implementation/ListenerServiceImpl.java
@@ -2,7 +2,26 @@ package com.github.vcamilx.staff.service.implementation;
 
 import com.github.vcamilx.staff.Staff;
 import com.github.vcamilx.staff.core.service.IService;
+import com.github.vcamilx.staff.listener.staff.StaffBlockBreakListener;
+import com.github.vcamilx.staff.listener.staff.StaffBlockPlaceListener;
+import com.github.vcamilx.staff.listener.staff.StaffChatListener;
+import com.github.vcamilx.staff.listener.staff.StaffDropItemListener;
+import com.github.vcamilx.staff.listener.staff.StaffInteractAtEntityListener;
+import com.github.vcamilx.staff.listener.staff.StaffInteractListener;
+import com.github.vcamilx.staff.listener.staff.StaffInventoryClickListener;
+import com.github.vcamilx.staff.listener.staff.StaffJoinListener;
+import com.github.vcamilx.staff.listener.staff.StaffPickupItemListener;
+import com.github.vcamilx.staff.listener.user.FreezeBreakBlockListener;
+import com.github.vcamilx.staff.listener.user.FreezeDamageByEntityListener;
+import com.github.vcamilx.staff.listener.user.FreezeDamageListener;
+import com.github.vcamilx.staff.listener.user.FreezeDropItemListener;
+import com.github.vcamilx.staff.listener.user.FreezeInventoryClickListener;
+import com.github.vcamilx.staff.listener.user.FreezeMoveListener;
+import com.github.vcamilx.staff.listener.user.FreezePickupItemListener;
+import com.github.vcamilx.staff.listener.user.FreezeProjectileLaunchEvent;
 import me.yushust.inject.InjectAll;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 
 @InjectAll
 @SuppressWarnings("unused")
@@ -10,7 +29,7 @@ public class ListenerServiceImpl implements IService {
 
     private Staff staff;
 
-    /*private StaffBlockBreakListener staffBlockBreakListener;
+    private StaffBlockBreakListener staffBlockBreakListener;
     private StaffChatListener staffChatListener;
     private StaffDropItemListener staffDropItemListener;
     private StaffInventoryClickListener staffInventoryClickListener;
@@ -32,13 +51,12 @@ public class ListenerServiceImpl implements IService {
     private void registerListener(Listener... listeners) {
         for (Listener listener : listeners) {
             Bukkit.getServer().getPluginManager().registerEvents(listener, staff);
-            Bukkit.getLogger().info(listener + " has been loaded");
         }
-    }*/
+    }
 
     @Override
     public void start() {
-        /*registerListener(
+        registerListener(
                 staffBlockBreakListener,
                 staffChatListener,
                 staffDropItemListener,
@@ -56,11 +74,16 @@ public class ListenerServiceImpl implements IService {
                 freezeMoveListener,
                 freezePickUpItemListener,
                 freezeDamageListener
-        );*/
+        );
+        Bukkit
+                .getLogger()
+                .info("All listeners has been enabled");
     }
 
     @Override
     public void stop() {
-
+        Bukkit
+                .getLogger()
+                .info("All listeners has been disabled");
     }
 }
