b172dd72b25671f7ef17166ec64285ac76e174b3
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-05-23 00:33:09 -0500

    Mongo has been removed

--- plugin/src/main/java/com/github/vcamilx/staff/listener/staff/StaffJoinListener.java
+++ plugin/src/main/java/com/github/vcamilx/staff/listener/staff/StaffJoinListener.java
@@ -1,6 +1,5 @@
 package com.github.vcamilx.staff.listener.staff;
 
-import com.github.vcamilx.staff.manager.database.MongoManager;
 import com.github.vcamilx.staff.manager.mode.StaffManager;
 import me.yushust.inject.InjectAll;
 import org.bukkit.entity.Player;
@@ -13,14 +12,12 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class StaffJoinListener implements Listener {
 
     private StaffManager staffManager;
-    private MongoManager mongoManager;
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         if (player.hasPermission("staff.mode")) {
             staffManager.setStaff(player);
-            mongoManager.connection();
         }
     }
 }
