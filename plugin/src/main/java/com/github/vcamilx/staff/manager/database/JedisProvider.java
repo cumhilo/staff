dfbcd9696a6c7a7e06ec9940bef5520f144f498c
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-05-26 08:57:03 -0500

    fixed null pointer exception

--- plugin/src/main/java/com/github/vcamilx/staff/manager/database/JedisProvider.java
+++ plugin/src/main/java/com/github/vcamilx/staff/manager/database/JedisProvider.java
@@ -1,28 +1,23 @@
 package com.github.vcamilx.staff.manager.database;
 
 import com.github.vcamilx.staff.Staff;
-import me.yushust.inject.InjectIgnore;
-import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import javax.inject.Inject;
 
-@SuppressWarnings("ConstantConditions")
 public class JedisProvider {
+
+    private final JedisPool pool;
+
     @Inject
-    private Staff staff;
+    public JedisProvider(Staff staff) {
+        this.pool = new JedisPool(
+                staff.getConfig().getString("database.redis.host"),
+                staff.getConfig().getInt("database.redis.port")
+        );
+    }
 
-    @InjectIgnore
-    private final JedisPool jedisPool = new JedisPool(
-            staff
-                    .getConfig()
-                    .getString("database.redis.host"),
-            staff
-                    .getConfig()
-                    .getInt("database.redis.port")
-    );
-
-    public Jedis getJedisPool() {
-        return jedisPool.getResource();
+    public JedisPool getJedisPool() {
+        return pool;
     }
 }
