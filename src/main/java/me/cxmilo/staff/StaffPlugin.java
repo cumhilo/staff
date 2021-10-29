d49eb08ed9089f99b4f9714cece591b57dc06e34
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-10-29 15:02:03 -0500

    project/code initialization

---
+++ src/main/java/me/cxmilo/staff/StaffPlugin.java
@@ -0,0 +1,17 @@
+package me.cxmilo.staff;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StaffPlugin
+        extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Initialize logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Shutdown logic
+    }
+}
