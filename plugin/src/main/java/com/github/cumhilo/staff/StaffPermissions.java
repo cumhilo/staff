750fe10df3aac248d0c69509fe87a290c0c3a48c
Cumhilo <72714741+cumhilo@users.noreply.github.com>
2024-07-07 21:37:46 -0500

    feat(permissions): add permission class handler

---
+++ plugin/src/main/java/com/github/cumhilo/staff/StaffPermissions.java
@@ -0,0 +1,12 @@
+package com.github.cumhilo.staff;
+
+public class StaffPermissions {
+    public static final String STAFF_ADMIN = "staff.*";
+    public static final String STAFF_MOD = "staff.mod";
+
+    public static final String STAFF_TELEPORT = "staff.command.teleport";
+    public static final String STAFF_TELEPORT_POSITION = "staff.command.teleport.position";
+    public static final String STAFF_TELEPORT_ALL = "staff.command.teleport.all";
+
+    public static final String BYPASS_COMMAND_COOLDOWN = "staff.command.cooldown.bypass";
+}
